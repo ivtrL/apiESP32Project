3dd6b4456309292def90dc6e1aacfeff013a8015
ivtrL <isaacvitorinola@outlook.com>
2023-11-01 11:18:39 -0300

    Added Middlewares

---
+++ src/middleware/verifyAccessJWT.middleware.ts
@@ -0,0 +1,31 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { users } from 'src/controllers/card.controller';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../../.env' });
+
+@Injectable()
+export class verifyAccessJWTMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    const userdb = users.find(
+      (user) => user.email === email && user.password === password,
+    );
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+    jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid access token' });
+      } else if (user != userdb) {
+        return res.status(403).json({ message: 'Invalid access token' });
+      }
+      req.body.user = user;
+    });
+    next();
+  }
+}
---
+++ src/middleware/verifyRefreshJWT.middleware.ts
@@ -0,0 +1,31 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { users } from 'src/controllers/card.controller';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../../.env' });
+
+@Injectable()
+export class verifyRefreshJWTMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    const userdb = users.find(
+      (user) => user.email === email && user.password === password,
+    );
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+    jwt.verify(token, process.env.REFRESH_SECRET_TOKEN, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid refresh token' });
+      } else if (user != userdb) {
+        return res.status(403).json({ message: 'Invalid refresh token' });
+      }
+      req.body.user = user;
+    });
+    next();
+  }
+}
