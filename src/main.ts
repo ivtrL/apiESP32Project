c20c06fc8920000aea10078cdd704072882bc1ef
Isaac Vitorino Lopes Alves <isaacvitorinola@outlook.com>
2023-11-20 14:37:27 -0300

    Fixed: port for render

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableShutdownHooks();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
